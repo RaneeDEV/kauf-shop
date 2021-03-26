94de11d49977955d38dec4b4840e342ff2135bba
Ranee <67798875+RaneeDEV@users.noreply.github.com>
2021-03-26 23:58:00 +0300

    Render delonghi (-json) page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
-"use strick"
+"use strick";
+
+let DELONGHI = [];
 const burgerBtn = document.getElementById("burgerBtn");
 const burgerMenu = document.getElementById("burgerMenu");
 const toTopLinkEl = document.getElementById("toTopLink");
 const mapIframeEl = document.getElementById("mapIframe");
+const productCardsWrapEl = document.getElementById("productCardsWrap");
+
+getData("/data/delonghi.json");
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      DELONGHI = data;
+      isertCardEl(productCardsWrapEl, DELONGHI);
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+const defaultnumber = new Intl.NumberFormat(undefined, {
+  maximumSignificantDigits: 3
+})
 
 // ========== BURGER BUTTON START ==========
 burgerBtn.addEventListener("click", () => {
@@ -64,3 +88,35 @@ const swiper = new Swiper(".swiper-container", {
   loop: true,
 });
 // ========== PRODUCT SLIDER END ==========
+
+function isertCardEl(whereEl, machines) {
+  let html = "";
+
+  machines.forEach((machines) => {
+    html += createCardsElement(machines);
+  });
+
+  whereEl.innerHTML = "";
+  whereEl.insertAdjacentHTML("beforeEnd", html);
+}
+
+function createCardsElement(machine) {
+  return `<div class="product-card">
+  <img
+    src="./img/product/delonghi/DELONGHI PRIMADONNA DELUXE 1.jpg"
+    alt="Delonghi PrimaDonna S"
+    height="1"  width="1" loading="lazy"
+  />
+  <h2 class="card-subtitle">
+    ${machine.type}
+    <br />
+    <span class="color-words card-model">${machine.make} ${machine.model}</span>
+  </h2>
+  <h3 class="card-price">${defaultnumber.format(machine.price)}₴</h3>
+  <p class="${
+    machine.available ? "availability" : "unavailability"}"></p>
+  <a href="product-page.html#${machine.id}"
+    ><button class="button">Подробнее</button></a
+  >
+</div>`;
+}
